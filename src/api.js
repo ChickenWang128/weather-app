56b1ea8a1f00b31cbd3bd42abc29a1d6178503a4
ChickenWang128 <yubinwang3@gmail.com>
2024-01-20 13:22:23 -0600

    added env

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'c34b5a8870mshec42807821e0d8cp1e9e01jsn820941dbbaf6',
+    'X-RapidAPI-Key': '${import.meta.env.VITE_RAPIDAPI_KEY}',
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
   },
 };
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-export const WEATHER_API_KEY = 'c342db84127a14abfd83303888af4901';
+export const WEATHER_API_KEY = '${import.meta.env.VITE_OPENWEATHER_KEY}';
